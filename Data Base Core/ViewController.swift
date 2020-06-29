98e7303cff8898f618d9988e327ec7c85da57b64
Vitalii Sosin <375693@mail.ru>
2020-06-30 00:27:06 +0300

    Initial Commit

---
+++ Data Base Core/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Data Base Core
+//
+//  Created by Виталий Сосин on 30.06.2020.
+//  Copyright © 2020 Vitalii Sosin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
